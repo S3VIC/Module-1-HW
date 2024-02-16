29cc17c76d28634f52e7f312169a766827921bea
Seweryn Kipa <sewk@simcorp.com>
2024-02-16 13:36:43 +0100

    Fixing conflicting changes

--- HelloWorldConsoleApp/Program.cs
+++ HelloWorldConsoleApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Another Hello, World! :)");
 Console.WriteLine("Feature branch 1 - development 1");
+Console.WriteLine("Feature branch 1 - development 2");
